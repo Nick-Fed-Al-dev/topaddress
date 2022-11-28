6c77c7bd9561f66bcda083fcaddbd75f4a092ef2
plattform-ersteller <plattform.ersteller@gmail.com>
2022-11-28 18:56:53 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, HashRouter} from "react-router-dom"
 
 import {Router} from "./Router.jsx"
 import {Header} from "./components/sections/Header.jsx"
@@ -8,8 +8,10 @@ import "./sass/index.sass"
 
 export const App = () => {
 
+    const RouterWrap = process.env.NODE_ENV === "production" ? HashRouter : BrowserRouter
+
     return (
-        <BrowserRouter>
+        <RouterWrap>
             <div>
                 <Header />
                 <main className="main">
@@ -17,6 +19,6 @@ export const App = () => {
                 </main>
                 <Footer />
             </div>
-        </BrowserRouter>
+        </RouterWrap>
     )
 }
